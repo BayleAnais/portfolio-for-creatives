a6efcac259d4915c9783bf6bdd944adfe86a7a83
BayleAnais <bayleanais@outlook.com>
2023-10-19 16:03:06 -0700

    more stuff

--- js/hero.js
+++ js/hero.js
@@ -1,17 +1,38 @@
 $(document).ready(function(){
 	$('#navigation-item-1').hover(function(){
-  		$('.overlay1').fadeIn(500);
+  		$('.overlay1').fadeIn(200);
   	},function(){
-  		$('.overlay1').fadeOut(500);
-  });
+  		$('.overlay1').fadeOut(200);
+	});
 	$('#navigation-item-2').hover(function(){
-  		$('.overlay2').fadeIn(500);
+  		$('.overlay2').fadeIn(200);
   	},function(){
-  		$('.overlay2').fadeOut(500);
-  });
+  		$('.overlay2').fadeOut(200);
+	});
 	$('#navigation-item-3').hover(function(){
-  		$('.overlay3').fadeIn(500);
+  		$('.overlay2').fadeIn(200);
   	},function(){
-  		$('.overlay3').fadeOut(500);
-  });  
+  		$('.overlay2').fadeOut(200);
+  });
+	$('#navigation-item-4').hover(function(){
+  		$('.overlay2').fadeIn(200);
+  	},function(){
+  		$('.overlay2').fadeOut(200);
+  });
+	$('#navigation-item-5').hover(function(){
+  		$('.overlay3').fadeIn(200);
+  	},function(){
+  		$('.overlay3').fadeOut(200);
+  }); 
+  	$('#navigation-item-6').hover(function(){
+	$('.overlay3').fadeIn(200);
+	},function(){
+	$('.overlay3').fadeOut(200);
+	}); 
+	
+
+  
+
+
+
 });
