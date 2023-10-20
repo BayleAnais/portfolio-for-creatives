3bce7290d7e65fd28f33d5213bf243e0debbaf20
BayleAnais <bayleanais@outlook.com>
2023-10-19 17:20:19 -0700

    homepage image links

---
+++ dist/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('#navigation-item-1').hover(function(){
+  		$('.overlay1').fadeIn(500);
+  	},function(){
+  		$('.overlay1').fadeOut(500);
+  });
+	$('#navigation-item-2').hover(function(){
+  		$('.overlay2').fadeIn(500);
+  	},function(){
+  		$('.overlay2').fadeOut(500);
+  });
+	$('#navigation-item-3').hover(function(){
+  		$('.overlay3').fadeIn(500);
+  	},function(){
+  		$('.overlay3').fadeOut(500);
+  });  
+});
--- js/hero.js
+++ js/hero.js
@@ -1,38 +1,50 @@
-$(document).ready(function(){
-	$('#navigation-item-1').hover(function(){
-  		$('.overlay1').fadeIn(200);
-  	},function(){
-  		$('.overlay1').fadeOut(200);
-	});
-	$('#navigation-item-2').hover(function(){
-  		$('.overlay2').fadeIn(200);
-  	},function(){
-  		$('.overlay2').fadeOut(200);
-	});
-	$('#navigation-item-3').hover(function(){
-  		$('.overlay2').fadeIn(200);
-  	},function(){
-  		$('.overlay2').fadeOut(200);
-  });
-	$('#navigation-item-4').hover(function(){
-  		$('.overlay2').fadeIn(200);
-  	},function(){
-  		$('.overlay2').fadeOut(200);
-  });
-	$('#navigation-item-5').hover(function(){
-  		$('.overlay3').fadeIn(200);
-  	},function(){
-  		$('.overlay3').fadeOut(200);
-  }); 
-  	$('#navigation-item-6').hover(function(){
-	$('.overlay3').fadeIn(200);
-	},function(){
-	$('.overlay3').fadeOut(200);
-	}); 
-	
-
-  
-
-
-
+$(document).ready(function () {
+  $("#navigation-item-1").hover(
+    function () {
+      $(".overlay1").fadeIn(200);
+    },
+    function () {
+      $(".overlay1").fadeOut(200);
+    }
+  );
+  $("#navigation-item-2").hover(
+    function () {
+      $(".overlay2").fadeIn(200);
+    },
+    function () {
+      $(".overlay2").fadeOut(200);
+    }
+  );
+  $("#navigation-item-3").hover(
+    function () {
+      $(".overlay3").fadeIn(200);
+    },
+    function () {
+      $(".overlay3").fadeOut(200);
+    }
+  );
+  $("#navigation-item-4").hover(
+    function () {
+      $(".overlay4").fadeIn(200);
+    },
+    function () {
+      $(".overlay4").fadeOut(200);
+    }
+  );
+  $("#navigation-item-5").hover(
+    function () {
+      $(".overlay5").fadeIn(200);
+    },
+    function () {
+      $(".overlay5").fadeOut(200);
+    }
+  );
+  $("#navigation-item-6").hover(
+    function () {
+      $(".overlay6").fadeIn(200);
+    },
+    function () {
+      $(".overlay6").fadeOut(200);
+    }
+  );
 });
