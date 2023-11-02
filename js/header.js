aa1fcf0f02cf90db985a2be7defe3569e69aa1dd
BayleAnais <bayleanais@outlook.com>
2023-11-01 19:22:22 -0700

    sitemap and updates

---
+++ js/header.js
@@ -0,0 +1,17 @@
+$(function () {
+  var lastScrollTop = 0,
+    delta = 15;
+  $(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+    if (st > lastScrollTop && lastScrollTop > 0) {
+      // downscroll code
+      $("header").css("top", "-10vh");
+    } else {
+      // upscroll code
+      $("header").css("top", "10vh");
+    }
+    lastScrollTop = st;
+  });
+});
